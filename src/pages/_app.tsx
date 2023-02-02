b67f777978957218b7b85d8d153ee321cfa45803
Jānis Alunāns <j.alunans@gmail.com>
2023-02-02 15:17:19 +0200

    Add skeleton navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import theme from "@/chakra/theme";
+
+import "@fontsource/open-sans";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/700.css";
+import Layout from "@/components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
