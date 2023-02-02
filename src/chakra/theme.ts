325efcbd83ec3a12296acdb908f6ab07a95ea2bf
Jānis Alunāns <j.alunans@gmail.com>
2023-02-02 15:16:33 +0200

    Add skeleton theming, fonts

---
+++ src/chakra/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF3c00",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  // NB: Chakra gives you access to `colorMode` and `theme` in `props`, this can be just defined with an object
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  components: {
+    // Componenents like buttons can be styled here
+  }, 
+});
+
+export default theme
