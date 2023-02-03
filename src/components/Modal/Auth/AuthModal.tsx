082f310ed05d0f99a8d149d6ca4c07b52129ffbf
Jānis Alunāns <j.alunans@gmail.com>
2023-02-03 19:32:35 +0200

    Close modal on succesful login

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,23 +1,41 @@
-import { AuthModalState } from '@/atoms/authModalAtom';
-import { Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
-import React from 'react';
-import { useRecoilState } from 'recoil';
-import AuthInputs from './AuthInputs';
-import OAuthButtons from './OAuthButtons';
+import { AuthModalState } from "@/atoms/authModalAtom";
+import { auth } from "@/firebase/clientApp";
+import {
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButtons";
 
-const AuthModal:React.FC = () => {
-    const [modalState, setModalState] = useRecoilState(AuthModalState)
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(AuthModalState);
+  const [user, loading, error] = useAuthState(auth);
 
-    function handleClose() {
-       setModalState(prev=>({
-        ...prev,
-        open: false
-       })) ;
+  function closeModal() {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  }
+
+  useEffect(() => {
+    if (user) {
+      closeModal();
     }
+  }, [user]);
 
   return (
     <>
-      <Modal isOpen={modalState.open} onClose={handleClose}>
+      <Modal isOpen={modalState.open} onClose={closeModal}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader textAlign={"center"}>
@@ -32,15 +50,22 @@ const AuthModal:React.FC = () => {
             justifyContent={"center"}
             pb={6}
           >
-            <Flex flexDir={"column"} align={"center"} justify={"center"} width={"75%"}>
-              <OAuthButtons/>
-              <Text color={"gray.400"} mt="3">OR</Text>
-              <AuthInputs/>
+            <Flex
+              flexDir={"column"}
+              align={"center"}
+              justify={"center"}
+              width={"75%"}
+            >
+              <OAuthButtons />
+              <Text color={"gray.400"} mt="3">
+                OR
+              </Text>
+              <AuthInputs />
             </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
     </>
   );
-}
+};
 export default AuthModal;
