c42ad324e1c6602b57e21aa741e4b2fdcde6df09
Jānis Alunāns <j.alunans@gmail.com>
2023-02-04 20:11:18 +0200

    Update navbar styling for better proportions

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/firebase/clientApp";
 import { Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import CommunityDropdown from "./CommunityDropdownMenu/CommunityDropdown";
 import RightSide from "./RightSide/RightSide";
 import SearchBox from "./SearchBox";
 
@@ -9,19 +10,18 @@ const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth)
 
   return (
-    <Flex bg="white" height="46px" padding="6px 12px">
-      <Flex gap="10px" align={"center"}>
+    <Flex bg="white" height="46px" padding="6px 12px" justifyContent="space-between" gap="10px">
+      <Flex gap="10px" alignItems={"center"}>
         <Image
           src="/images/pidgey.png"
           alt="website logo"
-          height="100%"
+          width="34px"
         ></Image>
         <Text display={{ base: "none", md: "unset" }}>Pidgitt</Text>
       </Flex>
-
+      {user && <CommunityDropdown/>}
       <SearchBox></SearchBox>
       <RightSide user={user}></RightSide>
-      {/* <Directory></Directory> */}
     </Flex>
   );
 };
