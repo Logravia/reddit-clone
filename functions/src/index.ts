a9a2489f76812b099b4d153e8bfc0e0521edab6a
Jānis Alunāns <j.alunans@gmail.com>
2023-02-06 18:21:27 +0200

    Add Firebase function to save User data

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUserDocument = functions.auth
+  .user()
+  .onCreate(async (user) => {
+
+    const newUser = {
+        username: "",
+        displayName: user.displayName,
+        email: user.email,
+        uid: user.uid,
+        pfp: "",
+        karma: 1,
+    }
+
+    db.collection("users")
+      .doc(user.uid)
+      .set(newUser);
+  });
