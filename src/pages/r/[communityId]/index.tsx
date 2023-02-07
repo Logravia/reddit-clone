8adbada9452e403b9c8036471a453551447a3469
Jānis Alunāns <j.alunans@gmail.com>
2023-02-07 20:12:24 +0200

    Set-up  server side rendering for community pages

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,37 +1,47 @@
-import { db } from '@/firebase/clientApp';
-import { doc, DocumentData, DocumentSnapshot, getDoc } from 'firebase/firestore';
-import { GetServerSidePropsContext } from 'next';
-import React from 'react';
+import { CommunityData } from "@/atoms/communityAtom";
+import { db } from "@/firebase/clientApp";
+import { Heading } from "@chakra-ui/react";
+import {
+  doc,
+  DocumentData,
+  DocumentSnapshot,
+  getDoc,
+} from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
 
 type CommunityPageProps = {
-   data: undefined 
+  commData: CommunityData;
 };
 
-const CommunityPage:React.FC<CommunityPageProps> = ({data}) => {
-    
-    return <div>There might be posts here one day</div>
-}
+const CommunityPage: React.FC<CommunityPageProps> = ({ commData }) => {
+  return <Heading>{commData.name}</Heading>;
+};
 
+// you must return a JSON-serializable object
+// no props with objects or undefined vals fror Next.js
 function pageProps(doc: DocumentSnapshot<DocumentData>) {
-    const props = {
-        data: doc.data()
-    }
-    return props
+  let data = doc.data() as CommunityData;
+  return {
+    props: {
+      commData: { ...data, creationDate: data.creationDate.nanoseconds },
+    },
+  };
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-        const communityRef = doc(
-          db,
-          "communities",
-          context.query.communityId as string
-        );
+  const communityRef = doc(
+    db,
+    "communities",
+    context.query.communityId as string
+  );
 
-    try {
-        const communityDoc = await getDoc(communityRef);
-        return pageProps(communityDoc);
-    } catch (e) {
-       console.log("getServerSideProps error", e) 
-    }
+  try {
+    const communityDoc = await getDoc(communityRef);
+    return pageProps(communityDoc);
+  } catch (e) {
+    console.log("getServerSideProps error", e);
+  }
 }
 
 export default CommunityPage;
