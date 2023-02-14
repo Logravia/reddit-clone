03e5ddf39bd56fd143cfeac080edaba4f4cfdc0f
Jānis Alunāns <j.alunans@gmail.com>
2023-02-14 21:03:42 +0200

    Clear userState atom on logout

--- src/components/Navbar/RightSide/LoggedInMenuItems.tsx
+++ src/components/Navbar/RightSide/LoggedInMenuItems.tsx
@@ -1,20 +1,30 @@
+import { userState } from "@/atoms/userAtom";
 import { auth } from "@/firebase/clientApp";
 import { MenuDivider, MenuItem, Text } from "@chakra-ui/react";
 import { signOut } from "firebase/auth";
 import React from "react";
 import { CgProfile } from "react-icons/cg";
 import { FiLogOut } from "react-icons/fi";
+import { useResetRecoilState } from "recoil";
 
 type LoggedInMenuItemsProps = {};
 
 const LoggedInMenuItems: React.FC<LoggedInMenuItemsProps> = () => {
+  const resetUserState = useResetRecoilState(userState)
+  
+  async function logOut() {
+    await signOut(auth);
+    resetUserState();
+  }
+
+
   return (
     <>
       <MenuItem icon={<CgProfile />}>
         <Text>Profile</Text>
       </MenuItem>
       <MenuDivider />
-      <MenuItem icon={<FiLogOut />} onClick={() => signOut(auth)}>
+      <MenuItem icon={<FiLogOut />} onClick={() => {logOut()} }>
         <Text>Logout</Text>
       </MenuItem>
     </>
