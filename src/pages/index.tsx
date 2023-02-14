a74a2ec3cf39894c55420a00665a70199822c662
Jānis Alunāns <j.alunans@gmail.com>
2023-02-14 21:01:01 +0200

    Update default index text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <p>Hello!</p>
+    <p>This is the default page</p>
   )
 }
